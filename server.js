f19c6bc6515f6673c7464d4fa3b04f2ca1744d58
Chris Yammine <cyammine91@gmail.com>
2016-01-03 15:05:12 -0800

    Move users routing to new file

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config');
-var db = require('./models/index');
-var User = require('./models/user')(db.sequelize, db.Sequelize);
 var bodyParser = require('body-parser');
 
 var app = express();
 var compiler = webpack(config);
+
+var userRoutes = require('./routes/api/user');
 
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath,
@@ -18,46 +18,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-// body-parser
+// Body parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// #index
-app.get('/users', function(req, res) {
-  User.findAll().then(function(users) {
-    res.json(users);
-  });
-});
-
-// #show
-app.get('/users/:id', function(req, res) {
-  User.findById(req.params.id).then(function(user) {
-    res.json(user);
-  });
-});
-
-// #create
-app.post('/users', function(req, res) {
-  // res.json({ key: req.body });
-  db.sequelize.sync().then(function() {
-    return User.create({
-      email: req.body.email,
-      password: req.body.password
-    });
-  }).then(function(user) {
-    res.json(user);
-  });
-});
-
-// #update
-app.put('/users/:id', function(req, res) {
-  User.findById(req.params.id).then(function(user) {
-    user.update(req.body).then(function() {
-      res.json({ test: "success"});
-    });
-  });
-});
-
+// Routing
+app.use('/api', userRoutes);
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
